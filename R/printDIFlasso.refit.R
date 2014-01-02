45e97fb32d57caf76d95e1c7dddafe9826870f03
Unknown author <unknown@unknown>
2014-01-02 00:00:00 +0000

    version 1.0-1

--- R/printDIFlasso.refit.R
+++
@@ -1,9 +0,0 @@
-printDIFlasso.refit <-
-function(dif.obj){
-  cat("Number of (valid) persons: P =",dif.obj$P,"\n")
-  cat("Number of items: I =",dif.obj$I,"\n")
-  cat("DIF Items:",dif.obj$dif.items,"\n")
-  cat("\n")
-  cat("Matrix of estimated item-specific coefficients (after refit):\n")
-  print.default(dif.obj$gamma)
-}
